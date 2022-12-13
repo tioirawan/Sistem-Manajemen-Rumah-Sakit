e825472ff569eba3a06e8a79b00ec954e8b918d2
indmind <sattanationmail@gmail.com>
2022-12-13 12:56:05 +0700

    first commit

---
+++ src/main/java/kel2/ti2a/sistemrumahsakit/SistemRumahSakit.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kel2.ti2a.sistemrumahsakit;
+
+/**
+ *
+ * @author tioirawan
+ */
+public class SistemRumahSakit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/kel2/ti2a/sistemrumahsakit/data/service/ILoginService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kel2.ti2a.sistemrumahsakit.data.service;
+
+/**
+ *
+ * @author tioirawan
+ */
+public interface ILoginService {
+    boolean login(String nIP, String password);
+}
