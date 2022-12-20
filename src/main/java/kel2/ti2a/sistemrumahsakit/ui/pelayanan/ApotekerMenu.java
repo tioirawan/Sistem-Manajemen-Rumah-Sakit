da1b98985a71a55a116d3868989f4f0673cd2b4a
Josafat Pratama Susilo <susilojosafat@gmail.com>
2022-12-21 00:24:52 +0700

    Resolve error when run

--- src/main/java/kel2/ti2a/sistemrumahsakit/ui/pelayanan/ApotekerMenu.java
+++ src/main/java/kel2/ti2a/sistemrumahsakit/ui/pelayanan/ApotekerMenu.java
@@ -75,7 +75,6 @@ public class ApotekerMenu extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(900, 600));
 
         searchButton.setText("Cari");
         searchButton.addMouseListener(new java.awt.event.MouseAdapter() {
